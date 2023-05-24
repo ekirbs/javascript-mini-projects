b8b1976978b72897c6f90b488ff5e6e0bab52ed2
Eric K <erickirberger@gmail.com>
2023-05-24 17:31:19 -0400

    connected all pages/components, put some base code to appear in components, running to bug test

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,6 +3,8 @@ import "./style.css";
 export default function Footer() {
 
   return (
-    <></>
+    <div className="Footer">
+      <h6>This is the footer</h6>
+    </div>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,6 +3,8 @@ import "./style.css";
 export default function Header() {
 
   return (
-    <></>
+    <div className="Header">
+      <h1>JavaScript Mini Projects</h1>
+    </div>
   );
 }
